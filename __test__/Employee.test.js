4c56b5adfac06eb3c27bbbc727c4fe2dbf5ed141
pdhende <pdhendework@gmail.com>
2022-03-08 22:42:50 -0500

    Updated Employee.test.js file to add test cases

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -2,11 +2,11 @@ const employee = require("../lib/Employee");
 
 describe('employee', () => {
 
+    const emp = new employee('John', 1, 'john@test.com');
+
     // Test 1 : Verify that the new object has all employee properties
     describe('Initialization', () => {
         it("should create an object with 'name', 'id' and 'email'", () => {
-            const emp = new employee('John', 1, 'john@test.com');
-            
             expect(emp).toEqual({name: 'John', id: 1, email: 'john@test.com'});
         });
     });
@@ -14,10 +14,32 @@ describe('employee', () => {
     // Test 2 : Check if function getName() returns name of employee
     describe('Get Name', () => {
         it("should return the name of the employee", () => {
-            const emp = new employee('John', 1, 'john@test.com');
             let empName = emp.getName();
             expect(empName).toEqual('John');
         });
     });
 
+    // Test 2 : Check if function getId() returns Id of employee
+    describe('Get Id', () => {
+        it("should return the id of the employee", () => {
+            let empId = emp.getId();
+            expect(empId).toEqual(1);
+        });
+    });
+
+    // Test 3 : Check if function getEmail() returns email of employee
+    describe('Get email', () => {
+        it("should return the email of the employee", () => {
+            let empEmail = emp.getEmail();
+            expect(empEmail).toEqual('john@test.com');
+        });
+    });
+
+    // Test 4 : Check if function getRole() returns role of employee
+    describe('Get role', () => {
+        it("should return the role of the employee", () => {
+            let empRole = emp.getRole();
+            expect(empRole).toEqual('Employee');
+        });
+    });
 });
