c0273eb0577019fae0ef951eb5f5c2819509b3b0
pdhende <pdhendework@gmail.com>
2022-03-08 23:20:34 -0500

    Updated Manager.test.js

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,11 +1,21 @@
 const employee = require('../lib/Employee');
 const manager = require('../lib/Manager');
 
-describe('Initialization', () => {
-    
-    // Test 1: Verify the new object has inherited employee poperties and has its own property
-    it("should create an object with 'name', 'id', 'email' and 'officeNumber'", () => {
-        const empManager = new manager('John', 1, 'john@test.com', 10);
-        expect(empManager).toEqual({name: 'John', id: 1, email: 'john@test.com', officeNumber: 10});
+describe('manager', () => {
+
+    const empManager = new manager('John', 1, 'john@test.com', 10);
+// Test 1: Verify the new object has inherited employee poperties and has its own property
+    describe('Initialization', () => {
+        it("should create an object with 'name', 'id', 'email' and 'officeNumber'", () => {
+            expect(empManager).toEqual({name: 'John', id: 1, email: 'john@test.com', officeNumber: 10});
+        });
+    });
+
+    // Test 2: Check if class method getRole is overriden and returns 'Manager'
+    describe('Get Role', () => {
+        it("should return the role as Manager", () => {
+            let empRole = empManager.getRole();
+            expect(empRole).toEqual('Manager');
+        });
     });
 });
